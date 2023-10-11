0247a40e5f35582313a5ad7da9f78fd004d72c11
kirill <kirill_pogudin@mail.com>
2023-10-11 12:16:07 +0500

    “SetupProject”

---
+++ AppNews/AppNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppNewsApp.swift
+//  AppNews
+//
+//  Created by Cyril on 11.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppNews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppNews
+//
+//  Created by Cyril on 11.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
